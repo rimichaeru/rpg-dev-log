8acf86395623269c88dcb4cd8c1408c943742494
rimichaeru <conceptual@protonmail.com>
2021-08-03 11:39:50 +0100

    added Vision page

--- src/containers/Vision/Vision.jsx
+++ src/containers/Vision/Vision.jsx
@@ -3,9 +3,30 @@ import styles from "./Vision.module.scss";
 
 const Vision = () => {
   return (
-    <>
-      <p>Vision works</p>
-    </>
+    <div className={styles.container}>
+      <h1>The Vision</h1>
+      <p>I love <span className="redDark">RNG</span>. And I want to endlessly explore. I want to be a debuffing rogue who can use a 2-handed scythe from stealth. But I want to start with <span className="redDark">nothing</span>, only to build myself over time to become a <span className="redDark">demi-god</span> gracing the land.</p>
+      <br />
+      <br />
+      <p>Personally, I want to infinitely invest into one character. This RPG starts with randomly generating loot and enemies and ends with randomly generating loot and enemies. I lied; this RNG RPG does not end, I want there to always be a reason to get stronger. I hope to give a meaningful and rewarding experience as you build your character; the thrill of anticipation towards your goals and the blooming accomplishment as you conquer them - <span className="redDark">those key precious feelings which have somehow died out in modern gaming.</span>
+      </p>
+      <br />
+      <br />
+      <p><span className="blueDark">This RPG aims to have all the classic elements, with an overarching plot rather than a direct one, and is set in the fantastical spanning nature of space - where else would be a better place to infinitely progress?
+      <br />
+      <br />
+      I want to capture the unique essence of good RNG RPG gaming, and so my quest has started.
+      <br />
+      <br />
+      My initial aims are; dynamic loot, practical skill trees, interesting enemies, 
+      <br />
+      multi-classing, and meaningful progression.
+      <br />
+      <br />
+      See You Soon!
+      </span></p>
+   
+    </div>
   );
 };
 
